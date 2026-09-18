1e34aa2fc0c54ab8f7f0b55a8fd1f5aefcfec228
RashidBik <abdurashid.bigzad1999@gmail.com>
2026-09-18 15:12:16 +0430

    chore: initial SvelteKit setup with RTL and Persian support

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} />
+	<title>سیستم مدیریت کلینیک</title>
+	<meta name="description" content="سیستم مدیریت ساده و هوشمند کلینیک" />
+</svelte:head>
+{@render children()}
